92ebe207844666807e7bd211dc3d240073cd805f
ZichengQu <37530692+ZichengQu@users.noreply.github.com>
2019-01-21 16:13:41 +0800

    Update JSP.java

--- note/JSP.java
+++ note/JSP.java
@@ -24,7 +24,7 @@ JSP: Java Server Pages/Java服务器页面
 					int i = 1;
 					System.out.println(i);
 				%>
-		2) <%=java变量或表达式%> 会被翻译成service方法内部的out.print();不能写分号，否则报错//<%=i+1 %>
+		2) <%=java变量或表达式%> 会被翻译成service方法内部的out.print();不能写分号，否则报错;	<%=i+1 %>
 		3) <%!java代码%> 声明变量和方法。会被翻译成servlet的成员内容;
 				<%!String name="Jack"; %>
 				<%!public void add(){
@@ -35,15 +35,15 @@ JSP: Java Server Pages/Java服务器页面
 		2)java注释: //单行注释 /*多行注释*/	可见范围: jsp源码，翻译后的servlet
 		3)jsp注释: <%--jsp注释 --%>			可见范围: jsp源码
 5.jsp的隐式对象:
-request:	HttpServletRequest 接口的实例
-response:	HttpServletResponse 接口的实例
-out:		JspWriter类的实例，用于把结果输出至网页上
-session:	HttpSession类的实例
-application:	ServletContext类的实例，与应用上下文有关
-config:		ServletConfig类的实例
-pageContext:	PageContext类的实例，提供对JSP页面所有对象以及命名空间的访问
-page:		类似于Java类中的this关键字
-Exception:	Exception类的对象，代表发生错误的JSP页面中对应的异常对象
+	request:	HttpServletRequest 接口的实例
+	response:	HttpServletResponse 接口的实例
+	out:		JspWriter类的实例，用于把结果输出至网页上
+	session:	HttpSession类的实例
+	application:	ServletContext类的实例，与应用上下文有关
+	config:		ServletConfig类的实例
+	pageContext:	PageContext类的实例，提供对JSP页面所有对象以及命名空间的访问
+	page:		类似于Java类中的this关键字
+	Exception:	Exception类的对象，代表发生错误的JSP页面中对应的异常对象
 6.jsp指令: jsp指令是指jsp翻译和运行的命令，jsp包括三大指令。
 	(1)page指令: 属性最多的指令(实际开发中page指令是默认的)，它会根据不同的属性，指导整个页面特性。
 		格式: <%@ page 属性名1="属性值" 属性名2="属性值" ...%>
@@ -112,3 +112,56 @@ Exception:	Exception类的对象，代表发生错误的JSP页面中对应的异
 8.转发和重定向的区别:
 	转发: 一次请求，一次响应; 地址栏的URL不会发生改变，在服务器端执行，所以必须在同一台服务器上; 转发的速度快;
 	重定向: 两次请求; 地址栏的URL改变为重定向后的那个URL，在客户端执行，所以可以在不同的服务器上;
+9.jsp九大内置对象/隐式对象:
+	(1)out: JspWriter对象，调用out.print()可以直接把字符串(可以是html和js等)打印到浏览器上;
+	(2)request:	HttpServletRequest接口的实例;向服务器发送请求;
+		常用方法: 
+			1)服务器的隐式请求对象(request)设置属性的方法: request.setAttribute(String str,Object obj);
+			2)获得属性值: request.getAttribute(String str);
+			3)跳转: request.getRequestDispatcher("路径").forward(request,response);//转发(url不变): 页面跳转，同时将数据传递到指定页面;
+			4)获得来自客户端的Cookie对象: 
+				Cookie: 存储到客户端的一个字符串;
+				Cookie实例: 保存用户名和密码;
+			5)向服务器放大权限(只能放大一级)后发送数据: request.getSession().setAttribute(String str,Object obj);
+			6)获取jsp页面的参数，通过参数得到其值: 获取表单提交数据和get方式地址栏参数
+				request.getParameter("参数名");
+				request.getParameterValues("参数名");
+	(3)response: HttpServletResponse接口的实例;响应服务器请求;
+		常用方法:
+			1)跳转: response.sendRedirect();/*重定向*/ 
+				与request跳转不同，request是服务器内部跳转，地址栏内部不发生变化，通过request的属性方法，在另一个jsp页面可以收到来自服务器的数据; response跳转为重新发送请求，地址栏发生变化，通过request的属性方法发送的数据必须放大权限才能在另一个jsp接收到数据。
+			2)增加Cookie: response.addCookie();
+	(4)session: HttpSession类的实例;会话，浏览器关闭即session关闭，不是关闭其中的某一个页面;
+		常用方法: session.setAttribute(String str,Object obj);	session.getAttribute(String str);
+	(5)application: ServletContext类的实例，与应用上下文有关;代表当前WEB应用，是ServletContext对象;
+		常用方法: application.setAttribute(String str,Object obj);	application.getAttribute(String str);
+	(6)pageContext: PageContext类的实例，提供对JSP页面所有对象以及命名空间的访问;
+		常用方法: pageContext.setAttribute(String str,Object obj);
+	(7)page: 指向当前jsp对应的Servlet对象的引用，但为Object类型，只能调用Object类的方法;类似于Java类中的this关键字;
+	(8)config: ServletConfig类的实例;当前ServletConfig对象;
+	(9)exception: Exception类的对象，代表发生错误的JSP页面中对应的异常对象,若无错误则exception为null;
+10.jsp的四大作用域: https://github.com/ZichengQu/Java/tree/JavaWeb/JSP/FourScopes/WebContent
+	(1)pageScope --> pageContext: 当前页面级，解决了当前页面的数据共享问题;获取其它内置对象(已经是内置，可直接获得，不需要用此方法);
+	(2)requestScope --> request: 请求级,一次请求的servlet的数据共享;
+	(3)sessionScope --> session: 会话级，一个用户的不同请求的数据共享;
+	(4)applicationScope --> application: 应用程序级，不同用户的数据共享问题;
+		session和application区别的例子: https://github.com/ZichengQu/Java/blob/JavaWeb/JSP/FourScopes/WebContent/countOnline.jsp
+			<body>
+				<%
+					//Integer num = (Integer)session.getAttribute("num");
+					Integer num = (Integer)application.getAttribute("num");
+					if(num==null||num==0){
+						//第一次访问页面
+						out.print("欢迎访问乾包网");
+						num = 1;
+					}else{
+						//返回访问值
+						out.print("欢迎再次访问");
+						num += 1;
+					}
+					//session.setAttribute("num", num);
+					application.setAttribute("num", num);//application是应用级，对于这个应用，数据共享;session是会话级，关闭浏览器或切换浏览器则数据重置;
+				%>
+				<p>页面访问量为:<%=num %></p>
+			</body>
+11.EmpManageByServlet(查询/模糊查询/删除，数据库与servlet和jsp的交互): https://github.com/ZichengQu/Java/tree/JavaWeb/JSP/EmpManageByServlet
